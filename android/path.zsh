422f8e482b77497d9536bf2c803835f1eb573cd1
Austin Kettner <austinkettner@gmail.com>
2017-08-19 17:53:41 -0400

    Update available paths

---
+++ android/path.zsh
@@ -0,0 +1,3 @@
+export ANDROID_HOME="/usr/local/share/android-sdk"
+export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
+export PATH=$PATH:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools
